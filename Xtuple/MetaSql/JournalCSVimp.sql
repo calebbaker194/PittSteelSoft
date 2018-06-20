4241337e56344b4a02177a3e229c1179487d8b12
Unknown <caleb.baker194@gmail.com>
2018-06-20 09:20:44 -0500

    SQL for CSVIMP

---
+++ Xtuple/MetaSql/JournalCSVimp.sql
@@ -0,0 +1,6 @@
+﻿UPDATE glseries
+SET glseries_accnt_id =( SELECT accnt_id 
+			 FROM accnt 
+			 WHERE ( CONCAT(accnt_number,accnt_sub)) = CAST(glseries_accnt_id AS TEXT)),
+    glseries_docnumber = ''
+WHERE glseries_docnumber = 'CSVimp'
